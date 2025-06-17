4afb03d6d812489bf77975bf0b48191618fafe66
Nhân Nguyễn Văn <dccmm123@gmail.com>
2025-06-17 17:40:17 +0000

    tiến hành review toàn bộ project. tiến hành bỏ những file khồng còn dùng

--- src/components/game/ActionButtons.tsx
+++ src/components/game/ActionButtons.tsx
@@ -1,15 +1,2 @@
-
-// This file is no longer used and can be deleted.
-// All actions have been moved to BottomNavBar.tsx or handled directly in page.tsx / FarmPlot.tsx.
-// If you have an advisor button or other actions you'd like to re-introduce,
-// they would need to be added to a different part of the UI.
-// For now, to avoid build errors if this file is imported somewhere,
-// I will leave it as an empty default export.
-
-import type { FC } from 'react';
-
-const ActionButtons: FC = () => {
-  return null;
-};
-
-export default ActionButtons;
+// This file is no longer used and its content has been removed.
+// Its functionality was integrated into other components like BottomNavBar.tsx.
