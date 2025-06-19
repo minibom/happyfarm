be9b4baafd4a844a6f66fad66dccfb5714a3f4c6
Nhân Nguyễn Văn <dccmm123@gmail.com>
2025-06-19 17:51:50 +0000

    dể đảm bảo tất cả các dịch vụ Firebase đều có thể sử dụng sự kiện của bạ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,9 +136,6 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
             `,
           }}
         />
